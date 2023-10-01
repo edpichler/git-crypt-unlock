da65a140443cc0262723d06eacae7ccfef51b882
Edu Pichler <edpichler@gmail.com>
2023-10-01 22:20:07 +0200

    Create REPOSITORY_PATH env.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,5 @@ gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
 
 /usr/libexec/gpg-preset-passphrase --preset --passphrase "$GPG_KEY_PASS" "$GPG_KEY_GRIP"
 
+cd "$REPOSITORY_PATH"
 git-crypt unlock
